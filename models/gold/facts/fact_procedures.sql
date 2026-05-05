a0fa2f9bb1e44429d7db10db527cbfe42bed20ff
kagiak <kagiak@gmail.com>
2026-05-05 16:32:39 +0200

    Update fact_procedures.sql

--- models/gold/facts/fact_procedures.sql
+++ models/gold/facts/fact_procedures.sql
@@ -1,13 +1,26 @@
+with proc as (
+    select *
+    from {{ ref('procedure_cost') }}
+),
+
+enc as (
+    select encounter_id, patient_id, payer_id
+    from {{ ref('stg_encounters') }}
+)
+
 select
-    patient,
-    encounter,
-    code,
-    base_cost,
-    duration_seconds,
-    cost_per_day,
-    year,
-    month_number,
-    month_name,
-    week_number,
-    day_name
-from {{ ref('procedures_cost') }}
+    p.procedure_id,
+    p.patient,
+    p.encounter,
+    p.code,
+    p.base_cost,
+    p.duration_seconds,
+    p.cost_per_day,
+    p.year,
+    p.month_number,
+    p.week_number,
+    p.day_name,
+    e.payer_id
+from proc p
+left join enc e
+  on p.encounter = e.encounter_id
