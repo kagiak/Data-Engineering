55281784a7a987db3262b7de6c78d24896b3b92a
kagiak <kagiak@gmail.com>
2026-05-05 15:15:34 +0200

    Update stg_encounters.sql

--- models/silver/stg_encounters.sql
+++ models/silver/stg_encounters.sql
@@ -1,16 +1,16 @@
 select 
-  Id,
-  START,
-  STOP,
-  PATIENT,
+  try_cast(Id as string) as Id,
+  cast(START as timestamp) as START,
+  cast(STOP as timestamp) as STOP,
+  try_cast(PATIENT as string) as PATIENT,
   ORGANIZATION,
   PAYER,
   ENCOUNTERCLASS,
-  CODE,
-  DESCRIPTION,
+  try_cast(CODE as integer) as CODE, 
+  initcap(trim(DESCRIPTION)) as DESCRIPTION,
   BASE_ENCOUNTER_COST,
   TOTAL_CLAIM_COST,
   PAYER_COVERAGE,
-  REASONCODE,
-  REASONDESCRIPTION
+  try_cast(REASONCODE as integer) as REASONCODE,
+  nitcap(trim(REASONDESCRIPTION)) as REASONDESCRIPTION
 from {{ ref('encounters_raw') }} 
