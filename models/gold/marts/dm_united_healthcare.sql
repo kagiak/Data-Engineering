3e231019fcacf624c0e9daf8b3775347d45db7f0
kagiak <kagiak@gmail.com>
2026-05-05 16:40:33 +0200

    Update dm_united_healthcare.sql

--- models/gold/marts/dm_united_healthcare.sql
+++ models/gold/marts/dm_united_healthcare.sql
@@ -1,11 +1,3 @@
-with united as (
-    select payer_id
-    from {{ ref('dim_payers') }}
-    where lower(payer_name) like '%united%'
-)
-
-select
-    fp.*
-from {{ ref('fact_procedures') }} fp
-join united u
-  on fp.payer_id = u.payer_i
+select *
+from {{ ref('fact_procedures') }}
+where lower(payer_name) like '%united%'
