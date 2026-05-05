455dfcc00ecdec7d4e8f3c241b418d5e959c1d8d
kagiak <kagiak@gmail.com>
2026-05-05 16:40:49 +0200

    Update dm_humana.sql

--- models/gold/marts/dm_humana.sql
+++ models/gold/marts/dm_humana.sql
@@ -1,11 +1,3 @@
-with humana as (
-    select payer_id
-    from {{ ref('dim_payers') }}
-    where lower(payer_name) like '%humana%'
-)
-
-select
-    fp.*
-from {{ ref('fact_procedures') }} fp
-join humana h
-  on fp.payer_id = h.payer_id
+select *
+from {{ ref('fact_procedures') }}
+where lower(payer_name) like '%humana%'
