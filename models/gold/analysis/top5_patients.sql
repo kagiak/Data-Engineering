3c2f89481dfaf5ce68843a86662b091a060f48a9
kagiak <kagiak@gmail.com>
2026-05-05 17:04:31 +0200

    Update top5_patients.sql

--- models/gold/analysis/top5_patients.sql
+++ models/gold/analysis/top5_patients.sql
@@ -1,7 +1,20 @@
+with patient_costs as (
+    select
+        fp.PATIENT as PATIENT_ID,
+        dp.FIRST,
+        dp.LAST,
+        sum(fp.BASE_COST) as TOTAL_COST
+    from {{ ref('fact_procedures') }} fp
+    left join {{ ref('dim_patients') }} dp
+      on fp.PATIENT = dp.PATIENT_ID
+    group by fp.PATIENT, dp.FIRST, dp.LAST
+)
+
 select
-    patient,
-    sum(base_cost) as total_cost
-from {{ ref('procedures_cost') }}
-group by patient
-order by total_cost desc
-limit 5;
+    PATIENT_ID,
+    FIRST,
+    LAST,
+    TOTAL_COST
+from patient_costs
+order by TOTAL_COST desc
+limit 5
