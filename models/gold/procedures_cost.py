abccc75e3d57b3fad4a2006afd9cc9af8bda26ff
kagiak <kagiak@gmail.com>
2026-05-05 15:41:18 +0200

    Delete models/gold/procedures_cost.py

--- models/gold/procedures_cost.py
+++
@@ -1,7 +0,0 @@
-def model(dbt, session):
-
-    df = dbt.ref("stg_procedures").to_pandas()
-    SECONDS_PER_DAY = 86400
-    df["cost_per_day"] = df["base_cost"] * (SECONDS_PER_DAY / df["duration_seconds"])
-
-    return df
