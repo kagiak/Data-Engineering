8b935fe4a4a6a9b58492d8cd5b253912ada65610
kagiak <kagiak@gmail.com>
2026-05-05 16:28:47 +0200

    Update procedures_cost.sql

--- models/silver/procedures_cost.sql
+++ models/silver/procedures_cost.sql
@@ -2,6 +2,6 @@ def model(dbt, session):
 
     df = dbt.ref("stg_procedures").to_pandas()
     SECONDS_PER_DAY = 86400
-    df["cost_per_day"] = df["base_cost"] * (SECONDS_PER_DAY / df["duration_seconds"])
+    df["COST_PER_DAY"] = df["base_cost"] * (SECONDS_PER_DAY / df["DURATION_SECONDS"])
 
     return df
