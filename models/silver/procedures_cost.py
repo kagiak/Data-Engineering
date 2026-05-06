d09f6e720f3dcd80c9260ed0e5c36752986b01be
kagiak <kagiak@gmail.com>
2026-05-06 16:44:58 +0200

    Update procedures_cost.py

--- models/silver/procedures_cost.py
+++ models/silver/procedures_cost.py
@@ -1,10 +1,10 @@
 def model(dbt, session):
     
-    -- configure table
+    # configure table
     dbt.config(materialized="table", alias="procedures_cost")
-    -- load input table
+    # load input table
     df = dbt.ref("stg_procedures")
-    -- filter table
+    # filter table
     filtered_df = df.filter(df.COST_PER_DAY > 30000)
 
     return filtered_df
