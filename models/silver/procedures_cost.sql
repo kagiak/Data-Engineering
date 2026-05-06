ba158fb4b41c9216cd74a8dc8a709f12c7332f65
kagiak <kagiak@gmail.com>
2026-05-06 16:30:42 +0200

    Update procedures_cost.sql

--- models/silver/procedures_cost.sql
+++ models/silver/procedures_cost.sql
@@ -4,10 +4,7 @@ def model(dbt, session):
     dbt.config(materialized="table", alias="procedures_cost")
     -- load input table
     df = dbt.ref("stg_procedures")
-    pdf = df.to_pandas()
+    -- filter table
+    filtered_df = df.filter(df.COST_PER_DAY > 30000)
 
-    -- calculate cost per day
-    SECONDS_PER_DAY = 86400
-    pdf["COST_PER_DAY"] = pdf["base_cost"] * (SECONDS_PER_DAY / pdf["DURATION_SECONDS"])
-
-    return pdf
+    return filtered_df
