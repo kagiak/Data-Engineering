0d47a56f5b19744b511e12f4d0a5cc5d008c5c6b
kagiak <kagiak@gmail.com>
2026-05-06 16:52:22 +0200

    Update dim_procedures.sql

--- models/gold/dimensions/dim_procedures.sql
+++ models/gold/dimensions/dim_procedures.sql
@@ -1,1 +1,12 @@
-select distinct * from {{ ref('stg_procedures') }}
+select distinct
+  PATIENT,
+  ENCOUNTER,
+  CODE,
+  BASE_COST,
+  DURATION_SECONDS,
+  COST_PER_DAY,
+  YEAR,
+  MONTH_NUMBER,
+  WEEK_NUMBER,
+  DAY_NAME
+from {{ ref('stg_procedures') }}
